16fc14c99fb8501d4a28a3caf7d4490b058fad21
Kaitlin Thachuk <kaitlinthachuk@gmail.com>
2020-06-08 17:13:03 -0700

    Add ability for players to update their crave and prestige points at the end of the game

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -7,6 +7,10 @@ function GameOver(props) {
     const { houses, house } = props;
     const [power, setPower] = useState([]);
     const [coins, setCoins] = useState([]);
+    const [crave, setCrave] = useState(0);
+    const [prestige, setPrestige] = useState(0);
+    const [updateCrave, setUpdateCrave] = useState(0);
+    const [updatePrestige, setUpdatePrestige] = useState(0);
 
     useEffect(() => {
         let powerPromises = [], coinsPromises = [];
@@ -65,43 +69,112 @@ function GameOver(props) {
             }
             setCoins(temp);
         })
+
+        database.ref('houses/' + house.key + "/crave").on('value', (snapshot) => {
+            setCrave(snapshot.val());
+        });
+
+        database.ref('houses/' + house.key + "/prestige").on('value', (snapshot) => {
+            setPrestige(snapshot.val());
+        });
     }, []);
 
+    function handleCraveUpdate(e) {
+        e.preventDefault();
+        setUpdateCrave(e.target.value);
+    }
+
+    function submitCrave(e) {
+        e.preventDefault();
+        database.ref('houses/' + house.key + "/crave").set(updateCrave);
+    }
+
+    function handlePrestigeUpdate(e) {
+        e.preventDefault();
+        setUpdatePrestige(e.target.value);
+    }
+
+    function submitPrestige(e) {
+        e.preventDefault();
+        database.ref('houses/' + house.key + "/prestige").set(updatePrestige);
+    }
+
     return (<div className="end-game-container">
-        <div className="power-container" key="power-container">
-            <table className="power" key="power">
-                <tbody>
-                    <tr>
-                        <th colSpan="2">
-                            Power
+        <div className="table-container">
+            <div className="power-container" key="power-container">
+                <table className="power" key="power">
+                    <tbody>
+                        <tr>
+                            <th colSpan="2">
+                                Power
                             </th>
-                    </tr>
-                    {power.map((val) => {
-                        return (<tr key={val}>
-                            <td>{val[1]}</td>
-                            <td>{val[0]}</td>
-                        </tr>)
-                    })}
-                </tbody>
-            </table>
-        </div>
-        <div className="coins-container" key="coins-container">
-            <table className="coins" key="coins">
-                <tbody>
-                    <tr>
-                        <th colSpan="2">
-                            Coins
+                        </tr>
+                        {power.map((val) => {
+                            return (<tr key={val}>
+                                <td>{val[1]}</td>
+                                <td>{val[0]}</td>
+                            </tr>)
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div className="coins-container" key="coins-container">
+                <table className="coins" key="coins">
+                    <tbody>
+                        <tr>
+                            <th colSpan="2">
+                                Coins
                             </th>
-                    </tr>
-                    {coins.map((val) => {
-                        return (<tr key={val}>
-                            <td>{val[1]}</td>
-                            <td>{val[0]}</td>
-                        </tr>)
-                    })}
-                </tbody>
-            </table>
+                        </tr>
+                        {coins.map((val) => {
+                            return (<tr key={val}>
+                                <td>{val[1]}</td>
+                                <td>{val[0]}</td>
+                            </tr>)
+                        })}
+                    </tbody>
+                </table>
+            </div>
+
         </div>
+        <div className="update-points">
+            <h3>Update Your Crave and Prestige Points!</h3>
+            <div className="update-crave">
+                <h4>Current Crave Points: {crave}</h4>
+                <input
+                    type="number"
+                    className="update-crave-input"
+                    name="crave-input"
+                    placeholder="0"
+                    onChange={handleCraveUpdate}
+                />
+                <input
+                    type="button"
+                    className="crave-button"
+                    name="crave-update"
+                    value="Update Crave!"
+                    onClick={submitCrave}
+                />
+            </div>
+            <div className="update-prestige">
+                <h4>Current Prestige Points: {prestige}</h4>
+                <input
+                    type="number"
+                    className="update-prestige-input"
+                    name="prestige-input"
+                    placeholder="0"
+                    onChange={handlePrestigeUpdate}
+                />
+                <input
+                    type="button"
+                    className="prestige-button"
+                    name="prestige-update"
+                    value="Update Prestige!"
+                    onClick={submitCrave}
+                />
+            </div>
+        </div>
+
     </div>)
 }
 
