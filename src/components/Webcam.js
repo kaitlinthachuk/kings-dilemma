b18f9f2b15f15fe78bb48d597fd7e44cd83d55fd
Kaitlin Thachuk <kaitlinthachuk@gmail.com>
2020-07-04 10:57:16 -0700

    Use Jitsi-react to add Jitsi to the project

--- src/components/Webcam.js
+++ src/components/Webcam.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
-// import Jitsi from 'react-jitsi';
+import Jitsi from 'react-jitsi';
 import { database } from '../firebase.js';
-
-// import '../styles/Webcam.scss';
 
 
 function Webcam(props) {
@@ -13,39 +11,13 @@ function Webcam(props) {
             setRoomName(snapshot.val());
         });
 
-        //     let iFrame = document.querySelector('iframe');
-
-        //     if (iFrame) {
-        //         let cssLink = document.createElement("link");
-        //         cssLink.href = "Webcam.scss";
-        //         cssLink.rel = "stylesheet";
-        //         cssLink.type = "text/css";
-        //         iFrame.contentDocument.head.appendChild(cssLink);
-        //    }
-
     }, []);
-
-    const domain = 'meet.jit.si';
-    const options = {
-        roomName: roomName,
-        configOverwrite: { startWithAudioMuted: true, startWithVideoMuted: true },
-        interfaceConfigOverwrite: { filmStripOnly: true, TOOLBAR_BUTTONS: [], SHOW_JITSI_WATERMARK: false },
-    };
-
-    // eslint-disable-next-line no-undef
-    let api = new JitsiMeetExternalAPI(domain, options);
-    const iframe = api.getIFrame();
-    let cssLink = document.createElement("link");
-    cssLink.href = "Webcam.scss";
-    cssLink.rel = "stylesheet";
-    cssLink.type = "text/css";
-    iframe.contentDocument.head.appendChild(cssLink);
-
-    console.log(iframe);
 
 
     return (<>
-        {props.isVisible && <div dangerouslySetInnerHTML={{ __html: iframe }} />}
+        {props.isVisible && <Jitsi roomName={roomName}
+            configOverwrite={{ startWithAudioMuted: true, startWithVideoMuted: true }}
+            interfaceConfigOverwrite={{ filmStripOnly: true, TOOLBAR_BUTTONS: [], SHOW_JITSI_WATERMARK: false }} />}
     </>
     )
 }
